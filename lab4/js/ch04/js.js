8658a186a102ac21ca372117763b554b114ed488
zhuru <83149098+zhurushu@users.noreply.github.com>
2022-01-01 19:25:31 +0800

    Add files via upload

---
+++ lab4/js/ch04/js.js
@@ -0,0 +1,314 @@
+"use strict";
+const {
+	vec3,
+	vec4,
+	mat4
+} = glMatrix;
+
+var gl;
+var buffer;
+var cBuffer
+var points = [];
+var index;
+var pointIndex = 0; //全部点的个数
+var graph = []; //记录图形
+var s = [1.0, 1.0, 1.0];
+var ds = 0.01; //缩放
+//颜色选择器
+var color = [0.0, 0.0, 0.0, 1.0];
+//三角形
+var trangle = [
+	-0.1, -0.1, 0,
+	0, 0.1, 0,
+	0.1, -0.1, 0
+]
+//矩形
+var rectangle = [
+	0.1, 0.1, 0.0,
+	0.1, -0.1, 0.0,
+	-0.1, -0.1, 0.0,
+	-0.1, 0.1, 0.0
+]
+var thetaLoc;
+var sLoc;
+var posLoc;
+
+var theta_r = 0.0;
+var theta_c = 0.0;
+
+function get(button) {
+	index = button.value;
+}
+var circle = [];
+
+function get_circle() {
+	var x = 0,
+		y = 0.1;
+	var a = 0,
+		b = 0;
+	circle.push(0.0, 0.0, x, y);
+	var degree = Math.PI / 180;
+	for (var i = 0; i < 360; i++) {
+		a = x * Math.cos(degree) - y * Math.sin(degree);
+		b = x * Math.sin(degree) + y * Math.cos(degree);
+		x = a;
+		y = b;
+		circle.push(a, b);
+	}
+	// console.log(circle);
+}
+
+function canvas_clear() {
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	canvas.width = canvas.width;
+}
+
+window.onload = function inti() {
+	var canvas = document.getElementById("canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	if (!gl) {
+		alert("WebGL isn't available");
+	}
+	get_circle();
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.enable(gl.DEPTH_TEST);
+
+	var program = initShaders(gl, "vertex-shader", "fragment-shader");
+	gl.useProgram(program);
+
+	buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, 16 * 100000, gl.STATIC_DRAW);
+
+	var Position = gl.getAttribLocation(program, "Position");
+	gl.vertexAttribPointer(Position, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(Position);
+
+	cBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, 16 * 100000, gl.STATIC_DRAW);
+
+	var Color = gl.getAttribLocation(program, "Color");
+	gl.vertexAttribPointer(Color, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(Color);
+
+	thetaLoc = gl.getUniformLocation(program, "theta");
+	sLoc = gl.getUniformLocation(program, "s");
+	posLoc = gl.getUniformLocation(program, "pos");
+
+	document.getElementById("cl").onchange = function(event) {
+		var c = 0;
+		var ch = 0;
+		var x = document.getElementById("cl").value;
+		var Scolor = x.colorRgb();
+		for (var i = 0; i < Scolor.length; i++) {
+			if (parseInt(Scolor[i]) <= 9 && parseInt(Scolor[i]) >= 0) {
+				c = 10 * c + parseInt(Scolor[i]);
+			} else {
+				color[ch++] = c;
+				c = 0;
+			}
+			if (i == Scolor.length - 1) {
+				color[ch++] = c;
+				c = 0;
+			}
+		}
+		color[3] = 1.0;
+		color[0] /= 255;
+		color[1] /= 255;
+		color[2] /= 255;
+		// console.log(color);
+	}
+
+	canvas.addEventListener("mousedown", function(event) {
+		var x = (event.clientX - canvas.width / 2) / (canvas.width / 2);
+		var y = -(event.clientY - canvas.height / 2) / (canvas.height / 2);
+		if (index == 0) {
+			mtrangle(x, y);
+		} else if (index == 1) {
+			mrectangle(x, y);
+		} else if (index == 2) {
+			mcube(x, y);
+		} else if (index == 3) {
+			mcircle(x, y);
+		}
+	})
+
+	render();
+}
+
+function mtrangle(x, y) {
+	points.push(x, y);
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	for (var i = 0; i < 3; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(glMatrix.vec4.fromValues(trangle[i *
+			3], trangle[i * 3 + 1], trangle[i * 3 + 2], 1.0)))
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	for (var i = 0; i < 3; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(color));
+	}
+	pointIndex += 3;
+	graph.push(0);
+}
+
+function mrectangle(x, y) {
+	points.push(x, y);
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	for (var i = 0; i < 4; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(glMatrix.vec4.fromValues(rectangle[i *
+			3], rectangle[i * 3 + 1], rectangle[i * 3 + 2], 1.0)))
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	for (var i = 0; i < 4; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(color));
+	}
+	pointIndex += 4;
+	graph.push(1);
+}
+
+function mcube(x, y) {
+	points.push(x, y);
+	var vertices = [
+		glMatrix.vec4.fromValues(-0.1, -0.1, 0.1, 1.0),
+		glMatrix.vec4.fromValues(-0.1, 0.1, 0.1, 1.0),
+		glMatrix.vec4.fromValues(0.1, 0.1, 0.1, 1.0),
+		glMatrix.vec4.fromValues(0.1, -0.1, 0.1, 1.0),
+		glMatrix.vec4.fromValues(-0.1, -0.1, -0.1, 1.0),
+		glMatrix.vec4.fromValues(-0.1, 0.1, -0.1, 1.0),
+		glMatrix.vec4.fromValues(0.1, 0.1, -0.1, 1.0),
+		glMatrix.vec4.fromValues(0.1, -0.1, -0.1, 1.0),
+	];
+	var vertexColors = [
+		glMatrix.vec4.fromValues(0.0, 0.0, 0.0, 1.0),
+		glMatrix.vec4.fromValues(1.0, 0.0, 0.0, 1.0),
+		glMatrix.vec4.fromValues(1.0, 1.0, 0.0, 1.0),
+		glMatrix.vec4.fromValues(0.0, 1.0, 0.0, 1.0),
+		glMatrix.vec4.fromValues(0.0, 0.0, 1.0, 1.0),
+		glMatrix.vec4.fromValues(1.0, 0.0, 1.0, 1.0),
+		glMatrix.vec4.fromValues(0.0, 1.0, 1.0, 1.0),
+		glMatrix.vec4.fromValues(1.0, 1.0, 1.0, 1.0)
+	];
+	var faces = [
+		1, 0, 3, 1, 3, 2, //正
+		2, 3, 7, 2, 7, 6, //右
+		3, 0, 4, 3, 4, 7, //底
+		6, 5, 1, 6, 1, 2, //顶
+		4, 5, 6, 4, 6, 7, //背
+		5, 4, 0, 5, 0, 1 //左
+	];
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	for (var i = 0; i < faces.length; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(vertices[faces[i]]));
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	for (var i = 0; i < faces.length; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(vertexColors[Math.floor(i / 6)]));
+	}
+	pointIndex += 36;
+	graph.push(2);
+}
+
+function mcircle(x, y) {
+	points.push(x, y);
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	for (var i = 0; i < 362; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(glMatrix.vec4.fromValues(circle[i *
+			2], circle[i * 2 + 1], 0.0, 1.0)));
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	for (var i = 0; i < 362; i++) {
+		gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (pointIndex + i), new Float32Array(color));
+	}
+	pointIndex += 362;
+	graph.push(3);
+}
+
+function render() {
+	var pindex = 0;
+	for (var i = 0; i < graph.length; i++) {
+		if (graph[i] == 3) {
+			var dx = Math.random() / 5;
+			var dy = Math.random() / 5;
+			var f = Math.random();
+			var f1 = Math.random();
+			if (f > 0.5) {
+				dx = -dx;
+			}
+			if (f1 > 0.5) {
+				dy = -dy;
+			}
+			gl.uniform3fv(posLoc, [points[i * 2] + dx, points[i * 2 + 1] + dy, 0.0]);
+		} else gl.uniform3fv(posLoc, [points[i * 2], points[i * 2 + 1], 0.0]);
+		if (graph[i] == 0) {
+			trender(pindex);
+			pindex += 3;
+		} else if (graph[i] == 1) {
+			rrender(pindex);
+			pindex += 4;
+		} else if (graph[i] == 2) {
+			crender(pindex);
+			pindex += 36;
+		} else if (graph[i] == 3) {
+			// console.log(1);
+			cirender(pindex);
+			pindex += 362;
+		}
+	}
+	requestAnimFrame(render);
+}
+
+function trender(pindex) {
+	s[0] = s[1] = s[2] += ds;
+	if (s[0] < 0.5) ds *= -1;
+	else if (s[0] > 2) ds *= -1;
+	gl.uniform3fv(sLoc, s);
+	gl.uniform3fv(thetaLoc, [0.0, 0.0, 0.0]);
+	gl.drawArrays(gl.TRIANGLES, pindex, 3);
+}
+
+function rrender(pindex) {
+	theta_r += 0.01;
+	if (theta_r > 2 * Math.PI) theta_r -= Math.PI * 2;
+	gl.uniform3fv(sLoc, [1.0, 1.0, 1.0]);
+	gl.uniform3fv(thetaLoc, [0.0, 0.0, theta_r]);
+	gl.drawArrays(gl.TRIANGLE_FAN, pindex, 4);
+}
+
+function crender(pindex) {
+	theta_c += 0.01;
+	if (theta_c > 2 * Math.PI) theta_c -= Math.PI * 2;
+	gl.uniform3fv(sLoc, [1.0, 1.0, 1.0]);
+	gl.uniform3fv(thetaLoc, [0.5, 0.5, theta_c]);
+	gl.drawArrays(gl.TRIANGLES, pindex, 36);
+}
+
+function cirender(pindex) {
+	gl.uniform3fv(sLoc, [1.0, 1.0, 1.0]);
+	gl.uniform3fv(thetaLoc, [0.0, 0.0, 0.0]);
+	gl.drawArrays(gl.TRIANGLE_FAN, pindex, 362);
+}
+
+var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+String.prototype.colorRgb = function() {
+	var sColor = this.toLowerCase();
+	if (sColor && reg.test(sColor)) {
+		if (sColor.length === 4) {
+			var sColorNew = "#";
+			for (var i = 1; i < 4; i += 1) {
+				sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+			}
+			sColor = sColorNew;
+		}
+		//处理六位的颜色值
+		var sColorChange = [];
+		for (var i = 1; i < 7; i += 2) {
+			sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)));
+		}
+		return sColorChange.join(",");
+	} else {
+		return sColor;
+	}
+}
---
+++ lab4/js/ch04/rotcube.js
@@ -0,0 +1,304 @@
+"use strict";
+const { vec3, vec4, mat4 } = glMatrix;
+
+var rotationMatrix;
+var rotationMatrixLoc;
+
+var angle = 0.0;
+var axis = [ 0, 0, 1 ];
+
+var trackingMouse = false;
+var trackballMove = false;
+
+var lastPos = [ 0, 0, 0 ];
+var curx, cury;
+var startx, starty;
+
+var canvas;
+var gl;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+// var axis = 0;
+// var theta = [0, 0, 0];
+// var thetaLoc;
+
+var maxis = -1;
+var t = 0;
+var d = [0.0, 0.0, 0.0];
+var dLoc;
+var dist = 0;
+function set_dist(){
+	dist = document.getElementById("dist").value;
+	// console.log(dist);
+}
+var s = 1;
+var smaxis = -1;
+var size = [1.0,1.0,1.0];
+var sizeLoc;
+function set_size(){
+	s = document.getElementById("size").value;
+}
+
+function initCube() {
+    canvas = document.getElementById("rtcb-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
+
+    makeCube();
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    // load shaders and initialize attribute buffer
+    var program = initShaders(gl, "rtvshader", "rtfshader");
+    gl.useProgram(program);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+	
+	rotationMatrix = mat4.create();
+	rotationMatrixLoc = gl.getUniformLocation( program, "rmat" );
+	gl.uniformMatrix4fv( rotationMatrixLoc, false, new Float32Array(rotationMatrix) );
+	
+	canvas.addEventListener( "mousedown", function(event){
+		var x = 2*event.clientX/canvas.width-1;
+		var y = 2*(canvas.height-event.clientY)/canvas.height-1;
+		startMotion( x, y );
+	});
+	
+	canvas.addEventListener( "mouseup", function(event){
+		var x = 2*event.clientX/canvas.width-1;
+		var y = 2*(canvas.height-event.clientY)/canvas.height-1;
+		stopMotion( x, y );
+	});
+	
+	canvas.addEventListener( "mousemove", function(event){
+		var x = 2*event.clientX/canvas.width-1;
+		var y = 2*(canvas.height-event.clientY)/canvas.height-1;
+		moveMotion( x, y );
+	});
+
+	//旋转方向
+    // thetaLoc = gl.getUniformLocation(program, "theta");
+    // gl.uniform3fv(thetaLoc, theta);
+    // document.getElementById("xbutton").onclick = function () {
+    //     axis = xAxis;
+    // }
+
+    // document.getElementById("ybutton").onclick = function () {
+    //     axis = yAxis;
+    // }
+
+    // document.getElementById("zbutton").onclick = function () {
+    //     axis = zAxis;
+    // }
+	
+	//平移方向
+	dLoc = gl.getUniformLocation(program, "d");
+	gl.uniform3fv(dLoc, d);
+	document.getElementById("move_x").onclick = function(){
+		maxis = xAxis;
+		t = 0.01;
+	}
+	document.getElementById("move_y").onclick = function(){
+		maxis = yAxis;
+		t = 0.01;
+	}
+	document.getElementById("move_z").onclick = function(){
+		maxis = zAxis;
+		t = 0.01;
+	}
+	document.getElementById("move_fx").onclick = function(){
+		maxis = xAxis;
+		t = -0.01;
+	}
+	document.getElementById("move_fy").onclick = function(){
+		maxis = yAxis;
+		t = -0.01;
+	}
+	document.getElementById("move_fz").onclick = function(){
+		maxis = zAxis;
+		t = -0.01;
+	}
+	
+	//缩放
+	sizeLoc = gl.getUniformLocation(program, "size");
+	gl.uniform3fv(sizeLoc, size);
+	document.getElementById("change_x").onclick = function(){
+		// console.log("x"+s);
+		smaxis = xAxis;
+	}
+	document.getElementById("change_y").onclick = function(){
+		// console.log("y"+s);
+	 	smaxis = yAxis;
+	} 
+	document.getElementById("change_z").onclick = function(){
+	 	smaxis = zAxis;
+		 console.log("z"+s);
+	 } 
+
+    render();
+}
+
+
+function trackballView( x, y ){
+	var d, a;
+	var v = [];
+
+	v[0] = x;
+	v[1] = y;
+
+	d = v[0]*v[0]+v[1]*v[1];
+	if( d < 1.0 )
+		v[2] = Math.sqrt( 1.0 - d );
+	else{
+		v[2] = 0.0;
+		a = 1.0 / Math.sqrt( d );
+		v[0] *= a;
+		v[1] *= a;
+	}
+	return v;
+}
+
+function startMotion( x, y ){
+	trackingMouse = true;
+	startx = x;
+	starty = y;
+	curx = x;
+	cury = y;
+
+	lastPos = trackballView( x, y );
+	trackballMove = true;
+}
+
+function moveMotion( x, y ){
+	var dx, dy, dz;
+
+	var curPos = trackballView( x, y );
+	if( trackingMouse ){
+		dx = curPos[0] - lastPos[0];
+		dy = curPos[1] - lastPos[1];
+		dz = curPos[2] - lastPos[2];
+
+		if( dx || dy || dz ){
+			angle = -1.0*Math.sqrt( dx*dx + dy*dy + dz*dz );
+
+			axis[0] = lastPos[1] * curPos[2] - lastPos[2] * curPos[1];
+			axis[1] = lastPos[2] * curPos[0] - lastPos[0] * curPos[2];
+			axis[2] = lastPos[0] * curPos[1] - lastPos[1] * curPos[0];
+
+			lastPos[0] = curPos[0];
+			lastPos[1] = curPos[1];
+			lastPos[2] = curPos[2];
+		}
+	}
+	render();
+}
+
+function stopMotion( x, y ){
+	trackingMouse = false;
+	if( startx != x || starty != y ){
+	}else{
+		angle = 0.0;
+		trackballMove = false;
+	}
+}
+
+
+function makeCube() {
+    var vertices = [
+        glMatrix.vec4.fromValues(-0.5, -0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(-0.5, 0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, 0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, -0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(-0.5, -0.5, -0.5, 1.0),
+        glMatrix.vec4.fromValues(-0.5, 0.5, -0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, 0.5, -0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, -0.5, -0.5, 1.0),
+    ];
+
+    var vertexColors = [
+        glMatrix.vec4.fromValues(0.0, 0.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 0.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 1.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(0.0, 1.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(0.0, 0.0, 1.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 0.0, 1.0, 1.0),
+        glMatrix.vec4.fromValues(0.0, 1.0, 1.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 1.0, 1.0, 1.0)
+    ];
+
+    var faces = [
+        1, 0, 3, 1, 3, 2, //正
+        2, 3, 7, 2, 7, 6, //右
+        3, 0, 4, 3, 4, 7, //底
+        6, 5, 1, 6, 1, 2, //顶
+        4, 5, 6, 4, 6, 7, //背
+        5, 4, 0, 5, 0, 1  //左
+    ];
+
+	// console.log(vertices[faces[0]][0],vertices[faces[0]][1],vertices[faces[0]][2]);
+    for (var i = 0; i < faces.length; i++) {
+        points.push(vertices[faces[i]][0], vertices[faces[i]][1], vertices[faces[i]][2]);
+
+        colors.push(vertexColors[Math.floor(i / 6)][0], vertexColors[Math.floor(i / 6)][1], vertexColors[Math.floor(i / 6)][2], vertexColors[Math.floor(i / 6)][3]);
+    }
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // theta[axis] += 0.05;
+    // gl.uniform3fv(thetaLoc, theta);
+	
+	if( trackballMove ){
+		vec3.normalize(axis,axis);//axis = normalize( axis );
+		//rotationMatrix = multiply( rotationaMatrix, rotate(angle, axis) );
+		var rmat=mat4.create();
+		mat4.fromRotation(rmat,angle,axis);
+		mat4.multiply(rotationMatrix, rotationMatrix, rmat);
+		gl.uniformMatrix4fv( rotationMatrixLoc, false, new Float32Array(rotationMatrix) );
+	}
+	
+	if(maxis != -1 && dist > 0 ){
+		dist -= 2;
+		d[maxis] += t;
+		gl.uniform3fv(dLoc, d);	
+	}else if(dist <= 0){
+		maxis = -1;
+	}
+	
+	if(smaxis != -1){
+		size[smaxis] = s;
+		gl.uniform3fv(sizeLoc, size);	
+		smaxis = -1;
+	}
+	
+    gl.drawArrays(gl.TRIANGLES, 0, points.length / 3);
+
+    requestAnimFrame(render);
+}
